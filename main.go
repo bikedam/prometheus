f8998c425270589647cdcc7c6b88542696b63ebb
GROUPINFRA\lubomir.sladok <lubomir.sladok@cgi.com>
2022-12-14 15:09:47 +0100

    add prometheus config

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "github.com/prometheus/client_golang/prometheus/promhttp"
+    "net/http"
+)
+
+
+
+func main() {
+    http.Handle("/metrics", promhttp.Handler())
+
+    http.ListenAndServe(":8000", nil)
+}
